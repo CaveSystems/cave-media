6bb2e89008479c82531f6c8218ad713caa18963e
Gernot Lenkner <g.lenkner@outlook.de>
2019-01-23 14:49:28 +0100

    added glfw3 specific event args, starting woth mouse button event.

---
+++ Cave.Media/OpenGL/glfw3.EventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cave.Media.OpenGL
+{
+    public static partial class glfw3
+    {
+        public class MouseButtonEventArgs : EventArgs
+        {
+            public Vector2 Position { get; set; }
+            public MouseButton Button { get; set; }
+            public InputState State { get; set; }
+            public KeyMods Mods { get; set; }
+        }
+    }
+}
