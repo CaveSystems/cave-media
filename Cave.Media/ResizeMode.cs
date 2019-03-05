e8e86bd8f5e5e32bbfac0fad763634580c1fb338
Gernot Lenkner <g.lenkner@outlook.de>
2019-03-05 08:27:02 +0100

    changes flags to pure enum, renamed resizeflags to resizemode

---
+++ Cave.Media/ResizeMode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cave.Media
+{
+	/// <summary>
+	/// Modes for resizing images
+	/// </summary>
+	public enum ResizeMode
+	{
+		/// <summary>Simple resize, exact fit in bounds</summary>
+		None = 0,
+
+		/// <summary>keep aspect, touch from insize</summary>
+		TouchFromInside = 1,
+
+        /// <summary>keep aspect, touch from outsize</summary>
+        TouchFromOutside = 2,
+    }
+}
