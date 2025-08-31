9422ecfaaebb096e4c0d8e62ee9e186106a5100c
Andreas Rohleder <39777727+Dingsd4@users.noreply.github.com>
2025-08-31 17:00:40 +0200

    Bugfix: SKBitmap.ToARGBImageData() now checks for correct SKColorType

--- Cave.Media.Skia/ARGBImageDataExtension.cs
+++ Cave.Media.Skia/ARGBImageDataExtension.cs
@@ -11,16 +11,24 @@ public static class ARGBImageDataExtension
     /// <summary>Loads the specified bitmap.</summary>
     /// <param name="bitmap">The bitmap.</param>
     /// <returns></returns>
-    public static ARGBImageData ToARGBImageData(this SKBitmap bitmap) => new ARGBImageData(bitmap.Bytes, bitmap.Width, bitmap.Height, bitmap.RowBytes);
+    public static ARGBImageData ToARGBImageData(this SKBitmap bitmap)
+    {
+        if (bitmap.ColorType != SKColorType.Rgba8888 || bitmap.AlphaType != SKAlphaType.Unpremul)
+        {
+            bitmap = new SKBitmap(bitmap.Width, bitmap.Height, SKColorType.Rgba8888, SKAlphaType.Unpremul);
+        }
+        return new ARGBImageData(bitmap.GetPixels(), bitmap.RowBytes * bitmap.Height, bitmap.Width, bitmap.Height, bitmap.RowBytes);
+    }
 
     /// <summary>Writes all data to a new <see cref="SKBitmap"/> instance</summary>
     /// <returns></returns>
     /// <exception cref="Exception">Invalid length!</exception>
     public static unsafe SKBitmap ToSKBitmap(this ARGBImageData imageData)
     {
+        var bitmap = new SKBitmap();
         var imgInfo = new SKImageInfo(imageData.Width, imageData.Height, SKColorType.Rgba8888, SKAlphaType.Unpremul);
-        var image = SKImage.FromPixels(imgInfo, (IntPtr)imageData.Pixels1);
-        return SKBitmap.FromImage(image);
+        bitmap.InstallPixels(imgInfo, imageData.Pointer, imageData.Stride);
+        return bitmap;
     }
 
     #endregion Public Methods
